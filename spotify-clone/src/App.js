63089692c2ebfa0b553ec1779f7f0a0db800f0fc
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-31 00:08:54 +0300

    logged in kontrolü

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../src/css/App.css';
 import Login from './component/Login';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
   // run code based on a given condition
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
     console.log('selam ben token', token);
+    if (_token) {
+      setToken(_token);
+    }
   }, []);
-  return (
-    <div className='app'>
-      <Login />
-    </div>
-  );
+  return <div className='app'>{token ? <h1>logged in</h1> : <Login />}</div>;
 }
 
 export default App;
