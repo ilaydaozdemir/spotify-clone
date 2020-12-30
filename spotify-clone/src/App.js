006204f32ecb716894c70bdcfad35a24942a9834
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-30 22:43:49 +0300

    run code based on a given condition

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,8 +1,14 @@
 import { useEffect } from 'react';
 import '../src/css/App.css';
 import Login from './component/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  // run code based on a given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log('selam ben token', token);
+  }, []);
   return (
     <div className='app'>
       <Login />
