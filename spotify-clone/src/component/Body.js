2bbce334c0dcd2cef214eb0cd82e5dea32371fc9
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-01 22:35:53 +0300

    dinamik yapı

--- spotify-clone/src/component/Body.js
+++ spotify-clone/src/component/Body.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import '../css/Body.css';
+import { useDataLayerValue } from './DataLayer';
 import Header from './Header';
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
   return (
     <div className='body'>
       <Header spotify={spotify} />
+      <div className='body_info'>
+        <img src={discover_weekly?.images[0].url} alt='' />
+        <div className='body_infoText'>
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
