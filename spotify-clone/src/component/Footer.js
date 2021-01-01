d2507393d19c5956825cf752aaff9bf460547942
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-01 17:38:39 +0300

    footer ıcon ekleme

--- spotify-clone/src/component/Footer.js
+++ spotify-clone/src/component/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../css/Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
 function Footer() {
   return (
@@ -8,7 +13,11 @@ function Footer() {
         <p>Album and song details</p>
       </div>
       <div className='footer_center'>
-        <p>Player controls</p>
+        <ShuffleIcon className='footer_green' />
+        <SkipPreviousIcon className='footer_icon' />
+        <PlayCircleOutlineIcon fontSize='large' className='footer_icon' />
+        <SkipNextIcon className='footer_icon' />
+        <RepeatIcon className='footer_green' />
       </div>
       <div className='footer_right'>
         <p>Volume controls</p>
