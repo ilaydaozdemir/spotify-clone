aa486bbc907d052544e07a60b036adbd04340e33
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-01 19:55:50 +0300

    adding avatar

--- spotify-clone/src/component/Header.js
+++ spotify-clone/src/component/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 import '../css/Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from './DataLayer';
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
   return (
     <div className='header'>
       <div className='header_left'>
@@ -11,8 +13,8 @@ function Header() {
         <input placeholder='Seacrh for Artist,Songs..' type='text' />
       </div>
       <div className='header_right'>
-        <Avatar src='' alt='RQ' />
-        <h4>İlayda Özdemir</h4>
+        <Avatar src={user?.images[0]?.url} alt='RQ' />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
